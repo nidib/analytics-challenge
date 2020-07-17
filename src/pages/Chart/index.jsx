318d77b718557a1b87241a236440fefeefdd4d59
Richard Bidin <richardbidin@outlook.com>
2020-07-17 12:00:01 -0300

    Added Chart page

---
+++ src/pages/Chart/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+const Chart = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Chart;
