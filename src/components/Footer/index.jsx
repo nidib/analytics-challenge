06d2349becf0aff5a742deea3a2232184431a432
Richard Bidin <richardbidin@outlook.com>
2020-07-18 23:23:41 -0300

    Added Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Developed by @richardbidins</p>
+    </footer>
+  );
+};
+
+export default Footer;
