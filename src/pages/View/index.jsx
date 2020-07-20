5d891fd7c525669369ed0837af574945a33b689f
Richard Bidin <richardbidin@outlook.com>
2020-07-19 22:44:32 -0300

    Added Logo, name & symbol

--- src/pages/View/index.jsx
+++ src/pages/View/index.jsx
@@ -1,38 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
-import stocks from '../../services/stocks';
 
+import Loading from '../../components/Loading';
 import Header from '../../components/Header';
 import CompanyTitle from '../../components/CompanyTitle';
 import RevenueChart from '../../components/Chart/RevenueChart';
 import EbitdaChart from '../../components/Chart/EbitdaChart';
+import GeneralBalance from '../../components/Chart/GeneralBalance';
 import Footer from '../../components/Footer';
 
 import api from '../../services/api';
 
 const View = () => {
   const [companyToBeViewed, setCompanyToBeViewed] = useState([]);
+  const [companyName, setCompanyName] = useState(undefined);
+  const [companyLogo, setCompanyLogo] = useState('');
+
   const params = queryString.parse(window.location.search);
   const { symbol } = params;
 
   useEffect(() => {
-    api.get(`v3/income-statement/${symbol}?apikey=50685ed6422c4ba4c0ff0875c2b76097`).then(((response) => setCompanyToBeViewed(response.data)));
+    api.get(`v3/financials/income-statement/${symbol}?apikey=109b3e1e77138ebe6cf237168a297c88`).then(((response) => {
+      setCompanyToBeViewed(response.data.financials);
+    }));
+    api.get(`v3/profile/${symbol}?apikey=109b3e1e77138ebe6cf237168a297c88`).then(((response) => {
+      setCompanyName(response.data[0].companyName);
+      setCompanyLogo(response.data[0].image);
+    }));
   }, []);
 
-  function getCompanyName(stockList) {
-    const [stockObjectFiltered] = stockList.filter((stockItem) => stockItem.symbol === symbol);
-    return (stockObjectFiltered) ? stockObjectFiltered.name : undefined;
+  if (!(companyName && companyToBeViewed)) {
+    return <Loading />;
   }
-  const companyName = getCompanyName(stocks);
 
   return (
     <>
       <Header />
-      {(!(symbol && companyName)) && <p className="error">There was an error loading the charts</p>}
       <main id="charts">
-        {(symbol && companyName) && <CompanyTitle companyName={companyName} companySymbol={symbol} />}
-        {(symbol && companyName) && <RevenueChart companyStock={companyToBeViewed} />}
-        {(symbol && companyName) && <EbitdaChart companyStock={companyToBeViewed} zero={10} />}
+        <CompanyTitle companyName={companyName} companySymbol={symbol} companyLogo={companyLogo} />
+        <RevenueChart companyStock={companyToBeViewed} />
+        <EbitdaChart companyStock={companyToBeViewed} />
+        <GeneralBalance companyStock={companyToBeViewed} />
       </main>
       <Footer />
     </>
