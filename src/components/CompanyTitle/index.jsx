48c830f7f048232d02abc09509433a200c501180
Richard Bidin <richardbidin@outlook.com>
2020-07-21 14:28:18 -0300

    Added stock comparison to Company Title

--- src/components/CompanyTitle/index.jsx
+++ src/components/CompanyTitle/index.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import numeral from 'numeral';
 import propTypes from 'prop-types';
 
 import './styles.css';
 
-const CompanyTitle = ({ companySymbol, companyName, companyLogo }) => {
+const CompanyTitle = ({ companiesProfiles }) => {
   return (
-    <div className="company-title">
-      <img src={companyLogo} alt={`${companySymbol} logo`} />
-      <h3>
-        {`${companyName} | ${companySymbol}`}
-      </h3>
-    </div>
+    <>
+      {
+        companiesProfiles.map((companyProfile, index) => {
+          return (
+            <div key={companyProfile.symbol}>
+              <div className="company-title">
+                <img src={companyProfile.image} alt={`${companyProfile.symbol} logo`} />
+                <h3>
+                  {`${companyProfile.companyName} | ${companyProfile.symbol}`}
+                  <span className="quote">{numeral(companyProfile.price).format('$0,0')}</span>
+                </h3>
+              </div>
+              {((companiesProfiles.length > 1) && (index < companiesProfiles.length - 1)) && <div className="vs">vs</div>}
+            </div>
+          );
+        })
+      }
+    </>
   );
 };
 
 CompanyTitle.propTypes = {
-  companySymbol: propTypes.string.isRequired,
-  companyName: propTypes.string.isRequired,
-  companyLogo: propTypes.string.isRequired,
+  companiesProfiles: propTypes.arrayOf(propTypes.object).isRequired,
 };
 
 export default CompanyTitle;
