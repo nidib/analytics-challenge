164174be97be669c9bc714bfca70b3c6e6dcc97f
Richard Bidin <richardbidin@outlook.com>
2020-07-21 14:10:04 -0300

    Removed dotenv dependency

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { config } from 'dotenv';
 import App from './App';
 
-config();
 ReactDOM.render(<App />, document.getElementById('root'));
