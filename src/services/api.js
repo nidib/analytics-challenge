060e169b5a24e30b0224659314f84dfab786c6d3
Richard Bidin <richardbidin@outlook.com>
2020-07-21 14:10:33 -0300

    Updated api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://financialmodelingprep.com/api/',
+  baseURL: 'https://financialmodelingprep.com/api/v3/',
 });
 
 export default api;
