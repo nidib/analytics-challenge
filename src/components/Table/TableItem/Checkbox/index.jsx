00eb8eb707b1a70b4d30d0abd723583bb550cb75
Richard Bidin <richardbidin@outlook.com>
2020-07-21 14:24:58 -0300

    Added elements to be viewed as widgets under the table

--- src/components/Table/TableItem/Checkbox/index.jsx
+++ src/components/Table/TableItem/Checkbox/index.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 
 import './styles.css';
 
+const listOfSelected = document.querySelector('ul#selected');
+
 function handleCheckboxClick({ target }) {
-  target.classList.toggle('checked');
+  target.classList.add('checked');
+  setTimeout(() => {
+    target.classList.remove('checked');
+  }, 500);
 
-  const compareButton = document.querySelector('.search-bar button');
+  const elementToBeSelected = document.createElement('li');
+  elementToBeSelected.classList.add('remove-selected');
+  elementToBeSelected.title = 'Remove item';
+  elementToBeSelected.id = target.parentElement.parentElement.id;
+  elementToBeSelected.innerText = target.parentElement.parentElement.id;
+  elementToBeSelected.addEventListener('click', (e) => {
+    e.target.parentElement.removeChild(e.target);
+  });
 
-  if (document.querySelectorAll('.checked').length >= 2) {
-    compareButton.classList.remove('disabled');
-  } else {
-    compareButton.classList.add('disabled');
+  if (!Array.from(listOfSelected.childNodes).some((child) => child.id === target.parentElement.parentElement.id) && Array.from(listOfSelected.childNodes).length < 3) {
+    listOfSelected.appendChild(elementToBeSelected);
   }
 }
 
