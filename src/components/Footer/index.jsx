ab085abbeb3cac08782799d482e1c204d4713f83
Richard Bidin <richardbidin@outlook.com>
2020-07-21 14:26:43 -0300

    Refactored Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 import React from 'react';
 
 import './styles.css';
@@ -5,7 +6,28 @@ import './styles.css';
 const Footer = () => {
   return (
     <footer>
-      <p>Developed by @richardbidins</p>
+      <div className="challenged-by">
+        <p>Challenged by&nbsp;</p>
+        <a href="https://github.com/ArturSch/desafio-frontend-se-analytics">SoftExpert</a>
+      </div>
+
+      <div className="developed-by">
+        <p>Developed by&nbsp;</p>
+        <a href="https://github.com/nidib">Richard Bidin</a>
+      </div>
+
+      <div className="used-apis">
+        <ul>
+          <li>
+            <p>Stock Rates & Financial Infos:&nbsp;</p>
+            <a href="https://financialmodelingprep.com/" title="Visit their website">Finacial Modeling Prep</a>
+          </li>
+          <li>
+            <p>Charts:&nbsp;</p>
+            <a href="https://www.chartjs.org/" title="Visit their website">Chart.js</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
