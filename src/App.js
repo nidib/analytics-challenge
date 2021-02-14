5f1276b75ef050eadfafb89ef5e403278ea86287
Richard Bidin <richardbidin@outlook.com>
2021-02-14 10:29:08 -0300

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+	render() {
+		return (
+			<h1>Hello World</h1>
+		);
+	}
+}
+
+App.displayName = 'App';
+
+export default App;
