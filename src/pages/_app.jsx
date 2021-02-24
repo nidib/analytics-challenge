d3578386472cbdb302c930f057ba30b4352f3949
Richard Bidin <richardbidin@outlook.com>
2021-02-24 18:20:31 -0300

    Fixed import

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import 'Styles/normalize.css';
-import 'Styles/globals.css';
+import 'styles/normalize.css';
+import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
