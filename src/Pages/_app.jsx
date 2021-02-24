4b068e25f76e786878e7a02a99db84f46f9354a9
Richard Bidin <richardbidin@outlook.com>
2021-02-24 13:31:04 -0300

    Fix _app.jsx

--- src/Pages/_app.jsx
+++ src/Pages/_app.jsx
@@ -4,7 +4,9 @@ import 'Styles/normalize.css';
 import 'Styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<Component {...pageProps} />
+	);
 }
 
 export default MyApp;
